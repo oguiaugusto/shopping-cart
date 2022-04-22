b8ecff45173f1ea1dabeb14cf50a9997aecd1355
Guilherme <guilhaugusto05@gmail.com>
2022-04-21 21:36:08 -0300

    feat: create function fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+
+const fetchProducts = async (query) => {
+  const response = await fetch(`${API_URL}${query}`);
+  const result = await response.json();
+
+  return result;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,7 +4,28 @@ const computadorSearch = require('../mocks/search');
 
 window.fetch = jest.fn(fetchSimulator);
 
-describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('fecthProducts: ', () => {
+  it('é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('quando passado o argumento "computador", fetch é chamada', async () => {
+    await fetchProducts('computador');
+    expect(window.fetch).toHaveBeenCalled();
+  });
+  it('quando chamada com o argumento "computador", utiliza o endpoint esperado', async () => {
+    await fetchProducts('computador');
+    expect(window.fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('quando chamada com o argumento "computador", retorna dados iguais aos esperados', async () => {
+    const result = await fetchProducts('computador');
+    expect(result).toEqual(computadorSearch);
+  });
+  it('quando chamada sem argumentos, lança um erro com a mensagem "You must provide an url"', async () => {
+    const expectedError = new Error('You must provide an url');
+    try {
+      await fetchProducts();
+    } catch (error) {
+      expect(error).toEqual(expectedError);
+    }
+  });
 });
